6a4650f5ed35c4b591defcfcfcf00051679bae0c
alanlgoncalves <alan.lgoncalves@outlook.com>
2025-06-30 13:45:23 -0300

    feat: Add delete example

---
+++ src/main/java/dev/alansantos/Example4.java
@@ -0,0 +1,31 @@
+package dev.alansantos;
+
+import dev.alansantos.conf.SparkConfiguration;
+import org.apache.spark.sql.SparkSession;
+
+public class Example4 {
+
+    /*
+        Exemplo de deleção de registros no Apache Iceberg
+     */
+    public static void main(String[] args) {
+
+        SparkSession.Builder sparkBuilder = SparkSession.builder()
+                .config(SparkConfiguration.getSparkConf());
+
+        try (final SparkSession sparkSession = sparkBuilder.getOrCreate()) {
+            dropIcebergTablePartition(sparkSession);
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void dropIcebergTablePartition(SparkSession sparkSession) {
+        sparkSession.sql("select * from local.flights where FLIGHT_DATE = '2015-01-01'").show(false);
+
+        sparkSession.sql("DELETE FROM local.flights WHERE FLIGHT_DATE = '2015-01-01'");
+
+        sparkSession.sql("select * from local.flights where FLIGHT_DATE = '2015-01-01'").show(false);
+    }
+
+}
